4fd619cdc10e06fb1d7fe21b4abcc80e6b02f1ef
reiniskr1 <Reinis.Krauja@gmail.com>
2024-11-28 23:47:30 +0100

    beatification and minor details

---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <v-app-bar flat color="#e0e0e0" elevation="1">
+    <v-container class="d-flex align-center py-0 px-6">
+      <div class="d-flex align-center">
+        <v-app-bar-title class="text-h5" color="white">
+          Kanban Board
+        </v-app-bar-title>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<style scoped></style>
--- src/components/KanbanGrid.vue
+++ src/components/KanbanGrid.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TaskCategory from "@/components/TaskCategory.vue";
 import { ref } from "vue";
+import AppBar from "@/components/AppBar.vue";
 
 let tasks = ref([
   [
@@ -57,40 +58,57 @@ const handleTasksReorder = (categoryIndex, newTasks) => {
 </script>
 
 <template>
-  <v-container>
-    <v-row>
-      <TaskCategory
-        :tasks="tasks[0]"
-        header-color="bg-red-accent-1"
-        title="Backlog"
-        @new-task="(task) => handleNewTask(0, task)"
-        @update-task="(task) => handleUpdatedTask(0, task)"
-        @update:tasks="(newTasks) => handleTasksReorder(0, newTasks)"
-      />
-      <TaskCategory
-        :tasks="tasks[1]"
-        header-color="bg-yellow-accent-1"
-        title="Doing"
-        @new-task="(task) => handleNewTask(1, task)"
-        @update-task="(task) => handleUpdatedTask(1, task)"
-        @update:tasks="(newTasks) => handleTasksReorder(1, newTasks)"
-      />
-      <TaskCategory
-        :tasks="tasks[2]"
-        header-color="bg-green-accent-2"
-        title="Review"
-        @new-task="(task) => handleNewTask(2, task)"
-        @update-task="(task) => handleUpdatedTask(2, task)"
-        @update:tasks="(newTasks) => handleTasksReorder(2, newTasks)"
-      />
-      <TaskCategory
-        :tasks="tasks[3]"
-        header-color="bg-blue-accent-1"
-        title="Done"
-        @new-task="(task) => handleNewTask(3, task)"
-        @update-task="(task) => handleUpdatedTask(3, task)"
-        @update:tasks="(newTasks) => handleTasksReorder(3, newTasks)"
-      />
-    </v-row>
-  </v-container>
+  <v-app>
+    <App-bar />
+    <v-main class="bg-grey-lighten-4">
+      <div class="bg-img">
+        <v-container class="bg-white-darken-3 bg-opacity-85">
+          <v-row>
+            <TaskCategory
+              :tasks="tasks[0]"
+              header-color="bg-red-accent-1"
+              title="Backlog"
+              @new-task="(task) => handleNewTask(0, task)"
+              @update-task="(task) => handleUpdatedTask(0, task)"
+              @update:tasks="(newTasks) => handleTasksReorder(0, newTasks)"
+            />
+            <TaskCategory
+              :tasks="tasks[1]"
+              header-color="bg-yellow-accent-1"
+              title="Doing"
+              @new-task="(task) => handleNewTask(1, task)"
+              @update-task="(task) => handleUpdatedTask(1, task)"
+              @update:tasks="(newTasks) => handleTasksReorder(1, newTasks)"
+            />
+            <TaskCategory
+              :tasks="tasks[2]"
+              header-color="bg-green-accent-2"
+              title="Review"
+              @new-task="(task) => handleNewTask(2, task)"
+              @update-task="(task) => handleUpdatedTask(2, task)"
+              @update:tasks="(newTasks) => handleTasksReorder(2, newTasks)"
+            />
+            <TaskCategory
+              :tasks="tasks[3]"
+              header-color="bg-blue-accent-1"
+              title="Done"
+              @new-task="(task) => handleNewTask(3, task)"
+              @update-task="(task) => handleUpdatedTask(3, task)"
+              @update:tasks="(newTasks) => handleTasksReorder(3, newTasks)"
+            />
+          </v-row>
+        </v-container>
+      </div>
+    </v-main>
+  </v-app>
 </template>
+<style scoped>
+.bg-img {
+  background:
+    linear-gradient(rgba(115, 115, 115, 0.9), rgba(115, 115, 115, 0.9)),
+    url("https://throughjuliaslens.com/wp-content/uploads/2018/06/Christiansborg5.jpg")
+      center center / cover;
+  min-height: 100vh;
+  position: relative;
+}
+</style>
